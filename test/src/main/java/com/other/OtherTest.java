1e2925221e17e86d987f06d1be7319ecad9c5b32
凤栖梧 <30561094+lt673701439@users.noreply.github.com>
2017-08-31 18:33:16 +0800

    在公司提交的测试

--- test/src/main/java/com/other/OtherTest.java
+++ test/src/main/java/com/other/OtherTest.java
@@ -3,5 +3,6 @@ package com.other;
 public class OtherTest {
     public static void main(String args[]){
         System.err.println("111");
+        System.err.println("公司");
     }
 }
--- test/src/main/java/com/test/TestRepo.java
+++ test/src/main/java/com/test/TestRepo.java
@@ -4,5 +4,6 @@ public class TestRepo {
     public static void main(String args[]){
         System.err.println("111");
         System.err.println("222");
+        System.err.println("公司");
     }
 }

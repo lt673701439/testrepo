6865b3f4b7d7f796617207da77a28a46e4170f06
凤栖梧 <673701439@qq.com>
2017-09-01 21:34:19 +0800

    test one

--- test/src/main/java/com/other/OtherTest.java
+++ test/src/main/java/com/other/OtherTest.java
@@ -3,6 +3,7 @@ package com.other;
 public class OtherTest {
     public static void main(String args[]){
         System.err.println("111");
+        System.err.println("222");
         System.err.println("公司");
     }
 }

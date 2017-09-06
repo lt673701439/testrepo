69bc26c72a437ba2207efe9af0eee99cd5ebfaff
凤栖梧 <30561094+lt673701439@users.noreply.github.com>
2017-09-06 09:38:24 +0800

    06在公司提交的测试,验证冲突

--- test/src/main/java/com/test/TestRepo.java
+++ test/src/main/java/com/test/TestRepo.java
@@ -2,7 +2,7 @@ package com.test;
 
 public class TestRepo {
     public static void main(String args[]){
-        System.err.println("111");
+        System.err.println("555");
         System.err.println("222");
         System.err.println("333");
         System.err.println("公司");

9e897b018e7230ac66a9ac215984077cd83e090c
凤栖梧 <673701439@qq.com>
2017-09-21 17:24:12 +0800

    3331

--- test/src/main/java/com/test/TestRepo.java
+++ test/src/main/java/com/test/TestRepo.java
@@ -2,8 +2,8 @@ package com.test;
 
 public class TestRepo {
     public static void main(String args[]){
-        System.err.println(222);
-        System.err.println("222");
+        System.err.println(333);
+        System.err.println(1);
         System.err.println("333");
         System.err.println("公司");
         System.err.println("home");

54408f3f4f2a4db857c20fbc5cb4451cae0381aa
liutong <liutong@winchannel.net>
2018-04-19 16:49:48 +0800

    fix conflict test

--- test/src/main/java/com/test/TestRepo.java
+++ test/src/main/java/com/test/TestRepo.java
@@ -3,9 +3,10 @@ package com.test;
 public class TestRepo {
     public static void main(String args[]){
         System.err.println(333);
-        System.err.println(1);
+        System.err.println(3);
         System.err.println(2);
         System.err.println(3);
-        System.err.println("home");
+        System.err.println("github");
+        System.err.println("github");
     }
 }

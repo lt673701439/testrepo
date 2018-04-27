02d74def0db7b9968664b6d4b453d97366652b4f
liutong <liutong@winchannel.net>
2018-04-27 10:58:35 +0800

    branch1 commit

--- test/src/main/java/com/test/TestRepo.java
+++ test/src/main/java/com/test/TestRepo.java
@@ -1,14 +1,11 @@
 package com.test;
 
 public class TestRepo {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.err.println(333);
         System.err.println(3);
         System.err.println(2);
         System.err.println(3);
-        System.err.println("github1");
-        System.err.println("github1");
-        System.err.println("work");
-        System.err.println("work");
+        System.err.println("branch1");
     }
 }

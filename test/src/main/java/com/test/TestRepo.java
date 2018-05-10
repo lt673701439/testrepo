4e5cc8981f3250d048ff1cac341e41d44bcc8294
liutong <liutong@winchannel.net>
2018-05-10 10:31:54 +0800

    master commit

--- test/src/main/java/com/test/TestRepo.java
+++ test/src/main/java/com/test/TestRepo.java
@@ -9,5 +9,6 @@ public class TestRepo {
         System.err.println("branch1");
         System.err.println("if has conflict");
         System.err.println("branch1 commit");
+        System.err.println("master commit");
     }
 }

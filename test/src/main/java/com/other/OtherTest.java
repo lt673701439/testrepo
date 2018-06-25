29e290a08242f902bd434fabe3d610984dc1d84c
liutong <liutong@winchannel.net>
2018-06-25 13:19:37 +0800

    eclipse

--- test/src/main/java/com/other/OtherTest.java
+++ test/src/main/java/com/other/OtherTest.java
@@ -5,8 +5,6 @@ public class OtherTest {
         System.err.println("555");
         System.err.println("222");
         System.err.println("公司");
-        System.err.println("idea");
-        System.err.println("idea");
         System.err.println("eclipse");
     }
 }

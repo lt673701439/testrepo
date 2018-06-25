cf3ee43e21e5320970869cac8b62c424bf0d91b9
liutong <liutong@winchannel.net>
2018-06-25 15:21:57 +0800

    delete

--- test/src/main/java/com/other/OtherTest.java
+++ test/src/main/java/com/other/OtherTest.java
@@ -1,11 +1,10 @@
 package com.other;
 
 public class OtherTest {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.err.println("555");
         System.err.println("222");
         System.err.println("公司");
-        System.err.println("eclipse");
         System.err.println("eclipse");
     }
 }
